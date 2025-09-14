3263167d16de9e7e6490eb2a4f1e45bb65f92321
jasa89 <78540352+jasa89@users.noreply.github.com>
2025-09-14 12:17:13 +0300

    search secure front and backend update1

--- client/src/screens/App.jsx
+++ client/src/screens/App.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header.jsx";
 import Search from "../components/Search.jsx";
-import Theater from "../components/Theater.jsx";
+
 
 export default function App() {
   return (
     <div>
       <Header/>
       <Search/>
-      <Theater/>
       <Outlet/>
     
     </div>
