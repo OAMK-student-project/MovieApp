e48dd14a8c4a9a8719c234725a7aa4ca10e1a80b
Zamperi <samuli.kokko@hotmail.com>
2025-09-24 18:07:57 +0300

    userRouter korjaus

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,3 +1,16 @@
+import { Router } from "express";
+import { signUp, signIn, refresh, signout } from "../controllers/userController.js";
+import { auth } from "../helpers/auth.js";
+
+const router = Router();
+
+router.post("/signup", signUp);
+router.post("/signin", signIn);
+router.post("/refresh", refresh);
+router.post("/signout", signout);
+
+export default router;
+
 import express from "express";
 import { pool } from "../helpers/db.js"; 
 
@@ -55,17 +68,5 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json({ error: "Database error" });
   }
 });
-
-export default router;
-import { Router } from "express";
-import { signUp, signIn, refresh, signout } from "../controllers/userController.js";
-import { auth } from "../helpers/auth.js";
-
-const router = Router();
-
-router.post("/signup", signUp);
-router.post("/signin", signIn);
-router.post("/refresh", refresh);
-router.post("/signout", signout);
 
 export default router;
