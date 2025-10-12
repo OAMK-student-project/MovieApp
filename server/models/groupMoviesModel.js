259e67b786838fa6bb6000acf1af06f071f21b8b
jasa89 <78540352+jasa89@users.noreply.github.com>
2025-10-12 16:00:05 +0300

    cleaned files

--- server/models/groupMoviesModel.js
+++ server/models/groupMoviesModel.js
@@ -1,42 +1,3 @@
-/*import db from '../helpers/db.js';
-//id auto-incremented
-
-//-----Get all Group Movies
-    const getAllMovies = async() => {
-        const result = await db.query('SELECT * FROM "Group_movies"')
-        return result.rows;
-    }
-
-//-----Get Group Movies by group id
-    const getMoviesById = async(groupMoviesId) => {
-        const result = await db.query('SELECT * FROM "Group_movies" WHERE group_id=$1', [groupMoviesId])
-        return result.rows;
-    }
-
-//-----Add Group Movies
-    //groupMoviesData = { group_id, movie_id, genre }
-    const addGroupMovies = async(groupMoviesData) => {
-        const timestamp = new Date();
-        const result = await db.query(
-            'INSERT INTO "Group_movies" (group_id, movie_id, genre, added_at) VALUES ($1, $2, $3, $4) RETURNING *',
-            [
-                groupMoviesData.group_id,
-                groupMoviesData.movies_id,
-                groupMoviesData.genre,
-                timestamp
-            ])
-        return result.rows;
-    }
-
-
-//-----Delete movie
-    const deleteGroupMovies = async(id) => {
-        const result = await db.query(
-            'DELETE FROM Group_movies WHERE id=$1 RETURNING *', [id])
-            return result.rows;
-    }
-
-export {  getAllMovies, getMoviesById, addGroupMovies, deleteGroupMovies }*/
 
 import db from '../helpers/db.js';
 
@@ -46,25 +7,7 @@ const getAllMovies = async () => {
   return result.rows;
 };
 
-/*// ----- Get movies by group ID -----
-const getMoviesById = async (groupId) => {
-  const result = await db.query('SELECT * FROM "Group_movies" WHERE group_id = $1', [groupId]);
-  return result.rows;
-};*/
 
-/*const getMoviesById = async (groupId) => {
-  const result = await db.query(
-    `SELECT 
-        id AS group_movie_id, 
-        movie_id, 
-        genre, 
-        added_at 
-     FROM "Group_movies" 
-     WHERE group_id = $1`,
-    [groupId]
-  );
-  return result.rows;
-};*/
 
 const getMoviesById = async (groupId) => {
   const result = await db.query(
