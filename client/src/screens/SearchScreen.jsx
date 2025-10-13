837c6e47b946b34de0366dfbe0956f7558063d42
Rieskamies <NLappalainen@protonmail.com>
2025-10-13 17:11:37 +0000

    Even more UI fixes and updates

--- client/src/screens/SearchScreen.jsx
+++ client/src/screens/SearchScreen.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
 import MovieCard from "../components/MovieCard"
+import './SearchScreen.css'
 
 export default function SearchScreen() {
     const location = useLocation();
     const results = location.state?.results || [];
   
     return (
-  
-     <div className="search-results-page">
-      <h2>Search results</h2>
-      <div className="movie-list">
-        {results.length === 0 && <p>No results</p>}
+      <div className="page">
+        <div className="search-results-page">
+          <h2 class="home-title">Search results</h2>
+          <div className="movie-list">
+            {results.length === 0 && <p>No results</p>}
 
-        {results.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+            {results.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
           </div>
-      </div>
+        </div>
+    </div>
   );
 }
 
